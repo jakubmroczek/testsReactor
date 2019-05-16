af535af80bc5dd3b8db1a2b5244294d3ea68b63d
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-05-16 16:57:22 +0200

    checked returned numbers of banknotes and type in payment

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,25 +1,51 @@
 package edu.iis.mto.testreactor.exc3;
 
+import static edu.iis.mto.testreactor.exc3.Banknote.PL100;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.lang.reflect.Parameter;
+
 public class AtmMachineTest {
 
-    @Mock
-    CardProviderService cardProviderService;
+    CardProviderService cardProviderService = mock(CardProviderService.class);
+    BankService bankService = mock(BankService.class);
+    MoneyDepot moneyDepot = mock(MoneyDepot.class);
 
-    @Mock
-    BankService bankService;
+    AtmMachine sut;
 
-    @Mock
-    MoneyDepot moneyDepot;
+    @Before
+    public void setUp() throws Exception {
+        sut = new AtmMachine(cardProviderService, bankService, moneyDepot);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test
+    public void paymentShouldContainDesiredAmountOfMoney() throws CardAuthorizationException{
+        Money money = Money.builder().withAmount(100).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("1234").withPinNumber(1234).build();
+
+        when(cardProviderService.authorize(any())).thenReturn(AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("1234").build());
+
+        Payment result = sut.withdraw(money, card);
+
+        assertThat(result.getValue().size(), is(1));
+        assertThat(result.getValue().get(0), is(PL100));
+    }
 }
